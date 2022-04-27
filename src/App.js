21855e3374ac1339374dccc203b2c20ac2d844d6
livos <lvivancos2nd-dev@yahoo.fr>
2022-04-27 15:15:18 +0700

    index pages

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
 import Error from "./pages/Error";
+import SharedLayout from "./pages/SharedLayout";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>our navbar</nav>
       <Routes>
-        <Route path="/" element={<Home />}>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
           <Route path="*" element={<Error />} />
         </Route>
-        <Route path="dashboard" element={<div>dashboard</div>}>
-          <Route path="stats" element={<div>stats</div>} />
-        </Route>
       </Routes>
-      <footer>our footer</footer>
     </BrowserRouter>
   );
 }
