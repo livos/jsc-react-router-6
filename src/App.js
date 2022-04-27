17e9d4823f64994bd65f81e57edb7e9910ae05ae
livos <lvivancos2nd-dev@yahoo.fr>
2022-04-27 12:37:55 +0700

    nested routes

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="*" element={<Error />} />
+        <Route path="/" element={<Home />}>
+          <Route path="about" element={<About />} />
+          <Route path="products" element={<Products />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+        <Route path="dashboard" element={<div>dashboard</div>}>
+          <Route path="stats" element={<div>stats</div>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
