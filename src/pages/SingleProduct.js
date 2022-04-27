499bea23365c313e03735c9e2d496a3ad2e99408
livos <lvivancos2nd-dev@yahoo.fr>
2022-04-27 17:07:33 +0700

    URL params

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,11 @@
+import { Link, useParams } from "react-router-dom";
+
 const SingleProduct = () => {
+  const { productId } = useParams();
   return (
-    <section className='section product'>
-      <h2>single product</h2>
+    <section className="section product">
+      <h4>{productId}</h4>
+      <Link to="/products">back to products</Link>
     </section>
   );
 };
